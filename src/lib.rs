0ca45573d5118c2619d28826f9932e0f5d48b542
Ian List <ian@fjodor.me>
2019-11-22 16:20:48 +0100

    Typetable will now be lazily evaluated and hidden behind static reference.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,26 @@
+#[macro_use]
+extern crate lazy_static;
+
 pub mod poke_type;
-pub mod generate_type_table;
+mod generate_type_table;
+
+use poke_type::TypeInteraction;
+use generate_type_table::generate_type_table;
+
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref TYPETABLE: HashMap<String, TypeInteraction> = {
+        match generate_type_table() {
+            Some(t) => t,
+            None => panic!()
+        }
+    };
+}
+
+pub fn recieve_type_table<'a>() -> &'a HashMap<String, TypeInteraction> {
+    &TYPETABLE
+}
 
 #[cfg(test)]
 mod tests {
