eb0260db6188907cc8c84b1dfeadcae394de087a
Ian List <ian@fjodor.me>
2019-11-23 11:40:40 +0100

    Can calculate what weaknesses are already countered in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
 
-pub mod poke_type;
+mod poke_type;
 mod generate_type_table;
 
 use poke_type::TypeInteraction;
@@ -22,10 +22,36 @@ pub fn recieve_type_table<'a>() -> &'a HashMap<String, TypeInteraction> {
     &TYPETABLE
 }
 
+pub fn calculate_weaknesses(team_types: Vec<String>) -> Result<Vec<String>, String> {
+    if team_types.len() < 1 && team_types.len() > 12{
+       return Err(String::from("Your team consists of either too many or too few different types"));
+    }
+    let mut all_weaknesses: Vec<String> = Vec::new();
+    let mut all_strengths: Vec<String> = Vec::new();
+    for pokemon_type in team_types {
+        match TYPETABLE.get(&pokemon_type) {
+            Some(type_interaction) => {
+                all_weaknesses.append(&mut type_interaction.get_weaknesses());
+                all_strengths.append(&mut type_interaction.get_strengths());
+            },
+            None => continue
+        };
+    }
+    let mut unblocked_weaknesses: Vec<String> = Vec::new();
+    for pokemon_type in all_weaknesses {
+        if all_strengths.contains(&pokemon_type) {
+            continue;
+        }
+        unblocked_weaknesses.push(pokemon_type);
+        
+    }
+    Ok(unblocked_weaknesses)
+}
+
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn it_works() {
-        assert_eq!(true, true);
     }
 }
